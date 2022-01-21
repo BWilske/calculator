f4903279a7b9db188da666da68fca9e1604db7d9
Brent Wilske <wilskeworkflow@gmail.com>
2022-01-20 20:15:34 -0800

    hooked up number buttons

--- app.js
+++ app.js
@@ -1,3 +1,68 @@
+
+
+let activeNum= []
+let activeNumIncrementer = 0
+let transferString = ""
+
+//when the odin project swings to objects, or another way to make this succinct come back and fix these
+
+const oneEl = document.getElementById("one-el")
+const twoEl = document.getElementById("two-el")
+const threeEl = document.getElementById("three-el")
+const fourEl = document.getElementById("four-el")
+const fiveEl = document.getElementById("five-el")
+const sixEl = document.getElementById("six-el")
+const sevenEl = document.getElementById("seven-el")
+const eightEl = document.getElementById("eight-el")
+const nineEl = document.getElementById("nine-el")
+const zeroEl = document.getElementById("zero-el")
+const addEl = document.getElementById("add-el")
+const subEl = document.getElementById("sub-el")
+const multEl = document.getElementById("mult-el")
+const divEl = document.getElementById("div-el")
+const decEl = document.getElementById("dec-el")
+const equalEl = document.getElementById("equal-el")
+const clearEl = document.getElementById("clear-el")
+const deleteEl = document.getElementById("del-el")
+const screenEl = document.getElementById("screen-el")
+
+
+oneEl.addEventListener("click", () => appendToActive(1))
+twoEl.addEventListener("click", () => appendToActive(2))
+threeEl.addEventListener("click", () => appendToActive(3))
+fourEl.addEventListener("click", () => appendToActive(4))
+fiveEl.addEventListener("click", () => appendToActive(5))
+sixEl.addEventListener("click", () => appendToActive(6))
+sevenEl.addEventListener("click", () => appendToActive(7))
+eightEl.addEventListener("click", () => appendToActive(8))
+nineEl.addEventListener("click", () => appendToActive(9))
+zeroEl.addEventListener("click", () => appendToActive(0))
+// addEl.addEventListener("click", setOperation(add) )
+// subEl.addEventListener("click", setOperation(sub) )
+// multEl.addEventListener("click", setOperation(mult) )
+// divEl.addEventListener("click", setOperation(div) )
+decEl.addEventListener("click", () => appendToActive("."))
+equalEl.addEventListener("click", () => runCalculations())
+// clearEl
+// deleteEl
+
+function appendToActive(input) {
+    if (!transferString){
+        activeNumIncrementer = activeNum.push(input)
+        activeNum.forEach(element => transferString += element)
+        screenEl.textContent = transferString
+        transferString = ""
+        console.log(activeNumIncrementer, activeNum, transferString)
+    }
+    else runCalculations();
+}
+
+
+
+
+
+
+
 let operation = {
     add: function(num1, num2) {
         return (num1 + num2)
